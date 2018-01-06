0d8f7651a3dde9499b585e0b9c22d928cdc822bf
Marco Botros <btsmarco@gmail.com>
2018-01-06 12:09:49 -0500

    added Rest framework to add api

--- ComeHome2/settings.py
+++ ComeHome2/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ComeHome.apps.ComehomeConfig',
+    'ComeHome',
+    'HomeApp_Rest',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
