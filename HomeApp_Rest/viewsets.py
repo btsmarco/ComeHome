8f328d987353ca2f38d77d79d8a9c8ada93f30a0
Marco Botros <btsmarco@gmail.com>
2018-01-25 23:53:20 -0500

    fixed refresh bug

--- HomeApp_Rest/viewsets.py
+++ HomeApp_Rest/viewsets.py
@@ -1,21 +1,30 @@
 from rest_framework import viewsets
 from HomeApp_Rest.serializers import PhotoSerializer
 from ComeHome.models import Photo
+from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import cache_control
 import random
-
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
 
 class RandPhotoViewSet(viewsets.ModelViewSet):
-    AllPhotos = Photo.objects.all()
-    # TODO get the first pk so we don't get a wrong one
-    # or do a retry when the pk doesn't exist
-    try:
-        randNum = random.randrange(1, len(AllPhotos))
-        Background = Photo.objects.get(pk=randNum)
-    except:
-        Background = Photo.objects.get(pk=len(AllPhotos))
-    queryset = [Background]
+    queryset = []
     serializer_class = PhotoSerializer
+
+    def __init__(self, *args, **kwargs):
+        ImagePhotosList = Photo.objects.values_list('image', flat=True)
+        pkPhotoList = Photo.objects.values_list('pk', flat=True)
+
+        randNum = random.randrange(1, len(pkPhotoList))
+
+        Background = {}
+        for index, p in enumerate(pkPhotoList):
+            if index == randNum:
+                Background = Photo.objects.get(pk=p)
+                break
+
+        self.queryset = [Background]
+        queryset = self.queryset
+        serializer_class = PhotoSerializer
